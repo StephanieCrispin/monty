5c233005aa1d7e9e0062340afd5b4ccd42a9d746
Georgen21 <chukwudinwabia42@gmail.com>
2023-10-18 02:30:10 -0700

    monty project

---
+++ pop_opcode.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * f_pop - prints the top
+ * @stack: stack head
+ * @line_number: line_number
+ * Return: void
+*/
+void f_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	h = *stack;
+	*stack = h->next;
+	free(h);
+}
