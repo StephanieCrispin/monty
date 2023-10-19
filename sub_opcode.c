c5172e5520b664904e617239cca1961747b77f88
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-10-19 08:53:29 +0100

    refactored sub

--- sub_opcode.c
+++ sub_opcode.c
@@ -6,22 +6,22 @@
  * @line_number: The line number
  */
 
-int check_length(stack_t *stack);
+int check_length_and_return_error(stack_t **stack, stack_t *head_copy, char *op, unsigned int line_number);
 void f_sub(stack_t **stack, unsigned int line_number)
 {
     int length = 0, result;
     stack_t *head_copy = *stack;
 
-    length = check_length(head_copy);
+    check_length_and_return_error(*stack, head_copy, "sub", line_number);
 
-    if (length < 2)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*stack);
-        exit(EXIT_FAILURE);
-    }
+    /* if (length < 2)
+     {
+         fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+         fclose(bus.file);
+         free(bus.content);
+         free_stack(*stack);
+         exit(EXIT_FAILURE);
+     }*/
 
     head_copy = *stack;
 
@@ -31,14 +31,23 @@ void f_sub(stack_t **stack, unsigned int line_number)
     free(head_copy);
 }
 
-int check_length(stack_t *stack)
+int check_length_and_return_error(stack_t **stack, stack_t *head_copy, char *op, unsigned int line_number)
 {
     int length = 0;
 
-    while (stack)
+    while (head_copy)
     {
-        stack = stack->next;
+        head_copy = head_copy->next;
         length++;
     }
     return (length);
+
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, op);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack);
+        exit(EXIT_FAILURE);
+    }
 }
